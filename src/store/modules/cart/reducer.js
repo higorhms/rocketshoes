628e7055a89d2ce60d950c090aa3c78db235d9f1
higorhms <higorhmscebrac@hotmail.com>
2019-11-04 22:25:35 -0300

    implementando o plugin do redux saga com o reactotron

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,14 +4,7 @@ export default function cart(state = [], action) {
     switch (action.type) {
         case '@Cart/ADD_SUCESS':
             return produce(state, draft => {
-                const productIndex = draft.findIndex(
-                    product => product.id === action.product.id
-                );
-                if (productIndex >= 0) {
-                    draft[productIndex].amount += 1;
-                } else {
-                    draft.push({ ...action.product, amount: 1 });
-                }
+                draft.push(action.product);
             });
         case '@Cart/REMOVE':
             return produce(state, draft => {
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,11 +1,26 @@
-import { call, all, takeLatest, put } from 'redux-saga/effects';
+import { call, all, takeLatest, put, select } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSucess } from './actions';
+import { addToCartSucess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
 
 export function* addToCart({ id }) {
-    const response = yield call(api.get, `/products/${id}`);
+    const productExists = yield select(state =>
+        state.cart.find(product => product.id === id)
+    );
 
-    yield put(addToCartSucess(response.data));
+    if (productExists) {
+        const amount = productExists.amount + 1;
+        yield put(updateAmount(id, amount));
+    } else {
+        const response = yield call(api.get, `/products/${id}`);
+
+        const data = {
+            ...response.data,
+            amount: 1,
+            priceFormatted: formatPrice(response.data.price),
+        };
+        yield put(addToCartSucess(data));
+    }
 }
 
 export default all([takeLatest('@Cart/ADD_REQUEST', addToCart)]);
