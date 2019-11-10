196a8758c98b22700df73f9a093883c65ef1ed21
higorhms <higorhmscebrac@hotmail.com>
2019-11-10 14:28:50 -0300

    Configurando o redux

---
+++ RocketShoesMOBILE/src/store/modules/Cart/reducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = [];
+
+export default function cart(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case 'ALGUMA ACAO AQUI':
+            return state;
+
+        default:
+            return state;
+    }
+}
